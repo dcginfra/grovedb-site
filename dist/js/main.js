5fb75fba499f12a60c71a92b64ebe7325c5396fe
Silvanasss <gothicdark.love@gmail.com>
2023-07-06 20:40:55 +0200

    feat:new-page

--- dist/js/main.js
+++ dist/js/main.js
@@ -10,25 +10,6 @@
       });
     }); 
   })(jQuery); 
-  // -- YouTube/Vimeo Frame Mobile Friendly --//
-  $(function() {
-    var $allVideos = $("iframe[src*='www.youtube.com'], iframe[src*='player.vimeo.com']");
-    $allVideos.each(function() {
-      $(this)
-        .data('aspectRatio', this.height / this.width)
-        .removeAttr('height')
-        .removeAttr('width');
-    });
-    $(window).resize(function() {
-      $allVideos.each(function() {
-        var $el = $(this);
-        var newWidth = $el.parent().width();
-        $el
-          .width(newWidth)
-          .height(newWidth * $el.data('aspectRatio'));
-      });
-    }).resize();
-  });
   // -- Mobile Button --//
   $(document).ready(function(){
     $("parent").click(function(){
